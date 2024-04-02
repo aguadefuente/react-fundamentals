e51b78223e8842d90ef0d7d0e27bd4947873ace7
lenilaurae <lenilaurae.gmail.com>
2024-04-02 18:38:46 +0200

    style

--- exercises/06.styling/03.problem.size-prop/index.tsx
+++ exercises/06.styling/03.problem.size-prop/index.tsx
@@ -6,9 +6,11 @@ function Box({
 	size, // 🐨 add a size prop here
 	...otherProps // 🦺 union this with an object that has a size prop type here which is
 	// optional and one of "small", "medium", or "large"
-}: {size?: "small" | "medium" | "large"} & React.HTMLAttributes<HTMLDivElement>) {
+}: {
+	size?: 'small' | 'medium' | 'large'
+} & React.HTMLAttributes<HTMLDivElement>) {
 	// 🐨 based on the size prop, define a new variable called sizeClassName
-	const sizeClassName = size ? `box--${size}` : "" //ternary operator
+	const sizeClassName = size ? `box--${size}` : '' //ternary operator - se lee: si zise es undefined entonces sizeClassName será "" sino será `box--${small o medium o large}`
 	return (
 		<div
 			// 🐨 add the sizeClassName to the className prop
@@ -24,13 +26,22 @@ function App() {
 		<div>
 			<p>size prop typescript</p>
 			{/* 🐨 update all these boxes to use the size prop */}
-			<Box /*className="box--small"*/ size="small" style={{ backgroundColor: 'lightblue' }}>
-				small lightblue box 
+			<Box
+				/*className="box--small"*/ size="small"
+				style={{ backgroundColor: 'lightblue' }}
+			>
+				small lightblue box
 			</Box>
-			<Box /*className="box--medium"*/ size="medium" style={{ backgroundColor: 'pink' }}>
+			<Box
+				/*className="box--medium"*/ size="medium"
+				style={{ backgroundColor: 'pink' }}
+			>
 				medium pink box
 			</Box>
-			<Box /*className="box--large"*/ size="large" style={{ backgroundColor: 'orange' }}>
+			<Box
+				/*className="box--large"*/ size="large"
+				style={{ backgroundColor: 'orange' }}
+			>
 				large orange box
 			</Box>
 			<Box>sizeless colorless box</Box>
